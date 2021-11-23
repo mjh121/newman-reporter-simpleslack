6556ff094d1cdb817e40863d8aeefcafbdcbe47b
mjh121 <jonghyeok.mun121@gmail.com>
2021-11-23 10:25:13 +0900

    version fix and add header argument

--- index.js
+++ index.js
@@ -10,6 +10,7 @@ const validationCheck = (webhookUrl) => {
 function SimpleSlackReporter(emitter, reporterOptions) {
     const webhookUrl = reporterOptions.webhookurl || process.env.SLACK_WEBHOOK_URL;
     const channel = reporterOptions.channel || process.env.SLACK_CHANNEL || '';
+    const header  = reporterOptions.header || process.env.SLACK_HEADER ||'';
     let imgLinkJson = reporterOptions.imglink || process.env.IMG_LINK || '';
     
     validationCheck(webhookUrl);
@@ -25,9 +26,10 @@ function SimpleSlackReporter(emitter, reporterOptions) {
         }
 
         const run = summary.run;
-        const collection = summary.collection;
+        const collection = summary.collection
+        const title =  (header == '') ? collection.name : [header, collection.name].join(': ');
 
-        SlackUtils.send(webhookUrl, SlackUtils.makeSlackMessage(run, collection.name,  channel, imgLinkJson));
+        SlackUtils.send(webhookUrl, SlackUtils.makeSlackMessage(run, title,  channel, imgLinkJson));
     });
 }
 
